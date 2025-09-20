407e379eb85631730130cd137b5a67943c46aaa9
mksmstpkc <mksmstpck@gmail.com>
2025-09-20 07:05:12 +0300

    feat(services): update services struct

--- internal/services/servieces.go
+++ internal/services/servieces.go
@@ -3,13 +3,17 @@ package services
 import "github.com/mksmstpck/spy_cat_agency/internal/db"
 
 type Services struct {
-	Breed  breed
-	SpyCat spyCat
+	Breed   breed
+	SpyCat  spyCat
+	Mission mission
+	Target  target
 }
 
 func NewServices(db db.DB) *Services {
 	return &Services{
-		Breed:  *newBreed(db),
-		SpyCat: *newSpyCat(db),
+		Breed:   *newBreed(db),
+		SpyCat:  *newSpyCat(db),
+		Mission: *newMission(db),
+		Target:  *newTarget(db),
 	}
 }
