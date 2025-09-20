597f19a265e5fc3efd5f97cc3f3848d588ad82a2
mksmstpkc <mksmstpck@gmail.com>
2025-09-20 07:04:53 +0300

    feat(db): update DB struct

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,13 +3,17 @@ package db
 import "github.com/jackc/pgx/v5/pgxpool"
 
 type DB struct {
-	Breed  breed
-	SpyCat spyCat
+	Breed   breed
+	SpyCat  spyCat
+	Mission mission
+	Target  target
 }
 
 func NewDB(conn *pgxpool.Pool) *DB {
 	return &DB{
-		Breed:  *newBreed(conn),
-		SpyCat: *newSpyCat(conn),
+		Breed:   *newBreed(conn),
+		SpyCat:  *newSpyCat(conn),
+		Mission: *newMission(conn),
+		Target:  *newTarget(conn),
 	}
 }
